f82f8b7a1abed5ffacff6679e9d87f009ab1d44d
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-12-06 14:58:11 -0700

    converted a value from one data type to another

--- Program.cs
+++ Program.cs
@@ -1,10 +1,38 @@
-﻿namespace TypeConversion;
+﻿using System;
 
-class Program
+namespace TypeConversion
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello, World!");
+        static void Main(string[] args)
+        {
+            /////// byte b = 1; // A byte is one byte and an int (integer) is four bytes///////
+            //int i = 1;
+            //byte b = 1;
+            //Console.WriteLine(i);
+
+
+            /////// Assigning an int value to an int variable/////////
+            ///// Attempting to assign the int value to a byte variable
+            //  This will result in a compilation error or data loss if not handled properly
+            //int i = 1000;
+            //byte b = (byte)i;
+            //Console.WriteLine(b);
+            try
+            {
+                string str = "true";
+                bool b = Convert.ToBoolean(str);
+                Console.WriteLine(b);
+                //var number = "1234";
+                //byte b = Convert.ToByte(number);
+                //Console.WriteLine(b);
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("THE NUMBER COULD NOT BE CONVERTED TO A BYTE.");
+            }
+
+        }
     }
 }
-
